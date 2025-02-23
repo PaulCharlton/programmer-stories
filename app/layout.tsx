4fdb9135874b0320667fc321d1c24581301472cc
Tallal <tallalmirza24@gmail.com>
2025-02-23 11:00:46 +0000

    theme mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { ThemeToggle } from "@/components/ThemeToggle";
 
 export const metadata: Metadata = {
   title: "Paul Charlton",
@@ -23,6 +24,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <ThemeToggle />
         </ThemeProvider>
       </body>
     </html>

02bd4d36acb161d259b4652d28d330a768973ce1
Tallal <tallalmirza24@gmail.com>
2025-03-14 15:36:22 +0100

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { ThemeToggle } from "@/components/ThemeToggle";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
   title: "Paul Charlton",
@@ -24,6 +25,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <Analytics />
           <ThemeToggle />
         </ThemeProvider>
       </body>
